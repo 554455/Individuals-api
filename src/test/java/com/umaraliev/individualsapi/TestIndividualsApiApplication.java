645c39aa5ee38d3e5b51b3bc0e5ce6a914b0ecb9
Бек <fazlidin.00@mail.ru>
2024-11-24 23:37:33 +0300

    Starting devepolement of the project

---
+++ src/test/java/com/umaraliev/individualsapi/TestIndividualsApiApplication.java
@@ -0,0 +1,11 @@
+package com.umaraliev.individualsapi;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestIndividualsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(IndividualsApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
